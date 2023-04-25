b41c40bebaa525722a7c33f467ac8b4942a7920e
Ming <world56@qq.com>
2023-04-25 22:25:47 +0800

    init

---
+++ service-gateway/index.js
@@ -0,0 +1,17 @@
+const fastify = require("fastify")({
+  logger: true,
+});
+
+fastify.register(require("@fastify/middie"));
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
